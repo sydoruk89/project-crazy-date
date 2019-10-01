b6fe8187618a9d1ac2a3ae8ae9f3f8b9728dffb8
Roman Sydoruk <sydoruk89@gmail.com>
2019-10-01 11:57:08 -0700

    js

--- js/app.js
+++ js/app.js
@@ -1,39 +1,51 @@
-/* eslint-disable no-unused-vars */
+'use strict';
 
-var formEl = document.getElementById('jsFormContainer');
-var city = ['Seattle', 'Tacoma', 'Olympia', 'Bellevue', 'Redmond'];
-var User = function (name, email, age, gender, orientation, city){
-    this.username = name,
+var User = function (name, email, gender, age, orientation, city){
+    this.name = name,
     this.email = email,
-    this.age = age,
     this.gender = gender,
+    this.age = age,
     this.orientation = orientation,
-    this.location = city;
-};
-user.prototype.addImage = function(path){
-    this.userImage = path;
-};
-user.prototype.ageRange = function(min, max){
-    this.ageMin = min,
-    this.ageMax = max;
+    this.city = city;
 };
 
+var usersArr = [];
 
-
-// Event listeners to add "new User"
-document.getElementById("submitUser").addEventListener("click", function(){
-    document.getElementById("name").innerHTML;
-    new User()
-}); 
-
-
-
-
-function addElement(element, elClass, content, parent){
-    var newElement = document.createElement(element);
-    if(elClass){
-        newElement.classList.add(elClass);
+function handleSubmit(event) {
+    event.preventDefault();
+    addInputs();
+    for(var i = 0; i < usersArr.length; i++){
+        if(usersArr[i].age >= 18) {
+            saveToLocalStorage();
+            showPreview();
+        } else{
+            alert('You are too small! You must be at least 18 yo!');
+        }
     }
+}
+
+function addInputs(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var gender = document.getElementById('gender').value;
+    var age = parseInt(document.getElementById('age').value);
+    var orientation = document.getElementById('orientation').value;
+    var city = document.getElementById('city').value;
+    var user = new User (name, email, gender, age, orientation, city);
+
+    usersArr.push(user);
+
+    return user;
+}
+
+// save to local storage
+var saveToLocalStorage = function() {
+    localStorage.setItem('users', JSON.stringify(usersArr));
+};
+
+// helper function
+function addElement(element, content, parent){
+    var newElement = document.createElement(element);
     if(content){
         var newContent = document.createTextNode(content);
         newElement.appendChild(newContent);
@@ -41,3 +53,118 @@ function addElement(element, elClass, content, parent){
     parent.appendChild(newElement);
     return newElement;
 }
+
+// show preview
+function showPreview() {
+    var divEl = document.getElementById('result');
+    var content = 'Thank you for filling out the form!';
+    addElement('h3', content, divEl);
+    addElement ('h4', 'Name:', divEl);
+    var name = document.getElementById('name').value;
+    addElement('p', name, divEl);
+    addElement('h4', 'Email:', divEl);
+    var email = document.getElementById('email').value;
+    addElement('p', email, divEl);
+    addElement('h4', 'I\'m:', divEl);
+    var gender = document.getElementById('gender').value;
+    addElement('p', gender, divEl);
+    addElement('h4', 'Age:', divEl);
+    var age = document.getElementById('age').value;
+    addElement('p', age, divEl);
+    addElement('h4', 'I\'m interested in:', divEl);
+    var orientation = document.getElementById('orientation').value;
+    addElement('p', orientation, divEl);
+    addElement('h4', 'City:', divEl);
+    var city = document.getElementById('city').value;
+    addElement('p', city, divEl);
+
+    document.getElementById('random').style.display = 'block';
+}
+
+// add event listener to the form
+
+var form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
+
+
+// render a random person to the page
+
+var imageOne = document.getElementById ('image1');
+
+
+var filepathArr = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
+
+var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+
+var allPersons = [];
+
+// construction function
+function Product (name, src){
+    this.name = name;
+    this.src = src;
+    this.votes = 0;
+    this.views = 0;
+
+    allPersons.push(this);
+}
+
+
+
+
+// helper function - calculate randon number
+function random(max){
+    return Math.floor(Math.random() * (max));
+}
+
+// render images
+function render(){
+    generateImg(imageOne);
+}
+
+// rendering pictures to the page
+function generateImg(domEl) {
+    var index = generateIndex();
+    allPersons[index].views++;
+    domEl.src = allPersons[index].src;
+    domEl.title = allPersons[index].name;
+    domEl.alt = allPersons[index].name;
+}
+
+// generating index
+function generateIndex() {
+    var index = random(allPersons.length);
+    return index;
+}
+
+function checkArr() {
+    for (var i = 0; i < filepathArr.length; i++) {
+        new Product(names[i],filepathArr[i]);
+    }
+}
+
+// clear function
+
+function clear(){
+    document.getElementById('div-form').innerHTML = '';
+}
+
+// welcome window
+function addWelcomeWindow(){
+    var divEl = document.getElementById('welcome-window');
+    var content = 'Based on your info we found the best match for you!';
+    addElement('h3', content, divEl);
+}
+// event handler
+function eventClick() {
+    clear();
+    addWelcomeWindow();
+    checkArr();
+    render();
+}
+
+// add event listener to the form
+
+var randomButton = document.getElementById('random');
+randomButton.addEventListener('click', eventClick);
